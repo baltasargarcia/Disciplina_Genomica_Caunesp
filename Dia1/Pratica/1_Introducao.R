7132d02b99b2b73c5214539b26296d46e96dfbb1
VitoMastrochirico <127856350+VitoMastrochirico@users.noreply.github.com>
2025-06-29 20:08:11 -0300

    Update 1_Introducao.R

--- Dia1/Pratica/1_Introducao.R
+++ Dia1/Pratica/1_Introducao.R
@@ -30,11 +30,12 @@ a
 b = 5
 b
 
-# criação de vetores
+# criação de vetores (concatenar elementos em um vetor)
 x <- c(1, 2, 3, 4, 5, 6) 
 x
 x2 <- 1:6
 x2
+# No R, colocamos texto (strings) entre aspas (' ' ou " ") para indicar que são valores de texto e não nomes de variáveis ou números.
 w <- c('an1', 'an2', 'an3')
 w
 
@@ -43,6 +44,7 @@ class(x)
 class(w)
 
 # Simulando valores com distribuição normal
+#cria um vetor y com 6 números gerados aleatoriamente de uma distribuição normal com média 10 e desvio padrão 5.
 y <- rnorm(6, mean = 10, sd = 5)
 y
 
@@ -53,6 +55,8 @@ help(rnorm)
 ##### Matrizes #####
 
 # Criando matrizes com colunas e linhas
+#cbind = combina vetores como colunas
+#rbind = combina vetores como linhas
 X <- cbind(x, y) # Atenção às letras maiúsculas e minúsculas
 X2 <- rbind(x, y)
 X
@@ -66,28 +70,32 @@ X2
 
 x3 <- c("low", "medium", "low", "high")
 x3
+
 x4 <- as.factor(x3)
 x4
-
+# as.factor = texto em variável categórica
 x5 <- factor(c("AA", "AT", "TT", "TT", "AT", "AT", "AA", "TT"))
 x5
 
 class(x3)
 class(x4)
 
-##### Operadores matemáticos básicos #####
+##### Operadores matemáticas básicas #####
 
+# vetores
 x <- 1:10
 y <- 11:20
+# O ; permite executar múltiplos comandos em uma linha
 x; y
 
+#operações com vetores
 z <- x + y
 z
 x * x
 x^2
 x / 2
 
-# Condições lógicas
+# Condições lógicas em vetores
 z2 <- x > 5
 z2
 z3 <- x == 3
@@ -125,13 +133,25 @@ set.seed(12345)
 x <- rnorm(1000, mean = 100, sd = 20)
 
 # Estatísticas básicas
+
+#média
 mean(x)
+
+#desvio padrão
 sd(x)
+
+# variância amostral de x, mede o quanto os valores se dispersam em torno da média, usando a fórmula: soma dos quadrados dos desvios dividida por (n - 1).
 var(x)
+
+#retorna um vetor com o valor mínimo e o valor máximo de x.
 range(x)
+
 min(x)
 max(x)
+
+#calcula os quantis de x para as probabilidades especificadas: 10%, 30%, 50% e 90%.
 quantile(x, prob = c(0.1, 0.3, 0.5, 0.9))
+
 summary(x)
 
 # Sumário de fator
@@ -143,8 +163,13 @@ plot(density(x))
 
 ##### Simulação de dados #####
 
+#tamanho da amostra
 N <- 30
+
+# gera 30 números aleatórios de uma distribuição normal padrão, (média 0, desvio padrão 1)
 x1 <- rnorm(N)
+
+#30 valores de uma distribuição binomial com size = 1 (sucesso ou fracasso, ex: 1 = infectado; 0 = resistente), com probabilidade de sucesso 0.2
 x2 <- rbinom(n = N, size = 1, prob = 0.2)
 
 # Tabela de frequências
@@ -158,9 +183,15 @@ tail(X)
 ##### Data frames #####
 
 # Criar data frame
+
 myData <- data.frame(Peso = x1, sex = x2)
+
+#dimensão do meu data frame
 dim(myData)
+
 head(myData)
+
+#estrutura do meu data frame
 str(myData)
 
 # Acessar colunas
