91bd752a2bdae8ef21cd8ecd73b0e82efde3db43
VitoMastrochirico <127856350+VitoMastrochirico@users.noreply.github.com>
2025-06-29 21:40:51 -0300

    Update 2_Estatisticas_basicas.R

--- Dia1/Pratica/2_Estatisticas_basicas.R
+++ Dia1/Pratica/2_Estatisticas_basicas.R
@@ -3,16 +3,25 @@ require(ggplot2)
 require(ggpubr)
 #install.packages("GGally")
 require(GGally)
-#Transformar los datos de .xls para .txt
+
 
 #Carpeta de trabajo
 setwd("C:/Users/Usuario/Desktop")
+
+#Transformar los datos de .xls para .txt
+
+#install.packages("readxl")
+library(readxl)
+
+dados <- read_excel("Desafio_IPN.xlsx")
+
+write.table(dados, file = "Desafio_IPN.txt", sep = "\t", row.names = FALSE)
 
 #Leyendo el archivo de datos
 data<-read.table("Desafio_IPN.txt",h=T)
 head(data)
 
-#estad?sticas descriptivas b?sicas
+#estadisticas descriptivas basicas
 mean(data$Mortal_weight)
 sd(data$Mortal_weight)
 summary(data$Mortal_weight)
@@ -22,14 +31,18 @@ sd(data$Mortal_DDM)
 summary(data$Mortal_DDM)
 
 
-#plots b?sicos
+#plots basicos
 plot(data$Mortal_weight)
 plot(data$Mortal_DDM)
 
 hist(data$Mortal_weight)
 hist(data$Mortal_DDM)
 
-#plot normalidad
+#plot normalidad 
+#compara la distribución de tus datos con una distribución normal — si los puntos caen cerca de una línea recta, indica que los datos son aproximadamente normales.
+#El eje x muestra los cuantiles teóricos de la distribución normal (valores esperados bajo normalidad).
+#El eje y muestra los cuantiles muestrales de tus datos.
+
 qqnorm(data$Mortal_weight)
 qqline(data$Mortal_weight)
 qqnorm(data$Mortal_DDM)
@@ -50,7 +63,7 @@ ggplot(data) +
   theme_minimal()+
 geom_smooth(mapping = aes(x = Tag_weight, y = Tag_length))
 
-#Edici?n de datos (removiendo datos)
+#Edición de datos (removiendo datos)
 data[data$Tag_weight > 4.9,]<-NA
 
 #Plot de correlaci?n
@@ -64,4 +77,8 @@ ggplot(data) +
 #Plot de correlacion con v?rias variables
 data2<-data[,c(10,11,14)]
 ggpairs(data2)
+
+#Diagonal: densidades de cada variable.
+#Debajo de la diagonal: gráficos de dispersión (scatterplots) que muestran la relación entre pares de variables.
+#Encima de la diagonal: coeficientes de correlación
   
